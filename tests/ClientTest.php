44026570f797d231bb28fae780ac5065d3f1f532
Greenreader9 <75644395+greenreader9@users.noreply.github.com>
2023-08-17 20:34:45 -0500

    Update ClientTest.php
    shut up automation

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -79,7 +79,7 @@ class ClientTest extends TestCase
 
         $this->guzzleMockHandler->append(new Response());
 
-        $response = $this->client->createAccount($username, $password, $email, $domain, $plan);
+        $response = $this->client->createAccount($username, $password, $email, $domain, $plan, $ipaddy);
         $this->assertInstanceOf(CreateAccountResponse::class, $response);
 
         $this->assertCount(1, $this->historyContainer);
